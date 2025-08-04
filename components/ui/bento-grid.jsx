cebce37f421773c17fcccf72cedeceef79275c49
Naresh Choudhary <nareshgorachiya@gmail.com>
2025-08-04 11:12:10 +0530

    some minor changes

--- components/ui/bento-grid.jsx
+++ components/ui/bento-grid.jsx
@@ -26,11 +26,12 @@ export const BentoGridItem = ({
   return (
     <div
       className={cn(
-        "group/bento shadow-input row-span-1 flex flex-col justify-between space-y-4 rounded-xl border border-neutral-200 bg-white p-4 transition duration-200 hover:shadow-xl dark:border-white/[0.2] dark:bg-black dark:shadow-none",
+        "group/bento shadow-input row-span-1 flex flex-col justify-between space-y-4 rounded-xl border border-neutral-200 bg-white p-4 hover:scale-[1.02] hover:shadow-xl ease-in-out dark:border-white/[0.2] dark:bg-black dark:shadow-none",
         className
-      )}>
+      )}
+      style={{ transition: 'all 0.1s ease-in-out' }}>
       {header}
-      <div className="transition duration-200 group-hover/bento:translate-x-2">
+      <div className="transition duration-200">
         <div
           className="mt-2 mb-2 font-sans font-bold text-neutral-600 dark:text-neutral-200">
           {title}
